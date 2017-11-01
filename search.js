598f12ae0a9ae2daffcec58d314b320a3269257f
Jahson <JahFriend@users.noreply.github.com>
2017-11-01 16:45:36 -0500

    Exercise 3
    Ths is exercise 3

--- search.js
+++ search.js
@@ -4,27 +4,34 @@ window.onload = function() {
   button.addEventListener("click", search);
 
   var httpRequest = new XMLHttpRequest();
-  var sourceUrl = "https://info2180-lab6-jahprogrammer.c9users.io/request.php?q=definition";
+  var sourceUrl = "https://info2180-lab6-jahprogrammer.c9users.io/request.php?q=";
 var answer = document.getElementById('result');
 
   function search() {
-	  httpRequest.onreadystatechange=function{
-		  
-		  
-		  if(httpRequest.readyState==XMLHttpRequest.DONE){
-			  if(httpRequest.status==200){
-				  var response== httpRequest.responseText';
-			  alert(response);}
-			  else{
-				  alert("Error");
-			  }
-				  
-  
+  var answer = document.getElementById('result');
+    var searchBy = document.getElementById("check").value.toLowerCase();
+    if (searchBy.length === 0) {
+      answer.innerHTML = "Text Field is empty";
+    } else {
+      var url = sourceUrl + searchBy;
+      /*alert(url);*/
+      httpRequest.open("GET", url, true);
+      httpRequest.send();
 
-  }
-}
+      httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
+            var response = httpRequest.responseText;
+            alert(response);
+            answer.innerHTML = response;
+          } else {
+            answer.innerHTML = "An error occured";
+          }
 
-httpRequest.open("GET",url,true);
-httpRequest.send();
+        }
+      }
+
+    }
+
   }
 }
