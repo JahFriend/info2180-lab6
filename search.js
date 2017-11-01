b9803717246f2b67ecf1e1c0b3c2b9c9fc251d38
Jahson <JahFriend@users.noreply.github.com>
2017-11-01 18:01:18 -0500

    Exercise 3
    This is exercise 3

--- search.js
+++ search.js
@@ -4,14 +4,14 @@ window.onload = function() {
   button.addEventListener("click", search);
 
   var httpRequest = new XMLHttpRequest();
-  var sourceUrl = "https://info2180-lab6-jahprogrammer.c9users.io/request.php?q=";
+  var sourceUrl = "https://lab-6-info-2180-jahprogrammer.c9users.io/request.php?q=";
 var answer = document.getElementById('result');
 
   function search() {
-  var answer = document.getElementById('result');
+  //  var answer = document.getElementById('result');
     var searchBy = document.getElementById("check").value.toLowerCase();
     if (searchBy.length === 0) {
-      answer.innerHTML = "Text Field is empty";
+      answer.innerHTML "Textfield is empty";
     } else {
       var url = sourceUrl + searchBy;
       /*alert(url);*/
